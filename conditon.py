2ade514f4e372593364ac9a4a1e3f94ce5b4b6ad
Namdev Kadam <namdevkadam6266@gmail.com>
2026-09-01 14:31:05 +0530

    Add condition checking for voting eligibility and string manipulation examples

---
+++ conditon.py
@@ -0,0 +1,5 @@
+age =52
+if(age>=18):
+    print("can vote & apply for license")
+elif(age<18):
+    print("u can not vote and not elegiable to apply license")
---
+++ str.py
@@ -0,0 +1,6 @@
+str1 = "this is str \nand it is used for only string"
+print(str1);
+print(len(str1))
+
+print("index of str1 30th ", str1[30])
+print(str1[1:6])
